1ef9ed4a44db984894b04d1b22905913570393e1
Mridul <dev.coffeebeans@gmail.com>
2018-08-06 09:39:49 +0530

    delete old python files

--- elliptical_drop/integrator.py
+++
@@ -1,9 +0,0 @@
-
-
-class Integrator:
-
-	def __init__(self):
-		pass
-
-
-	
--- elliptical_drop/utilities.py
+++
@@ -1,7 +0,0 @@
-
-from math import sqrt
-
-def euclidean_distance(x_1, y_1, x_2, y_2):
-	return sqrt((x_1-x_2)**2 + (y_1-y_2)**2)
-
-
