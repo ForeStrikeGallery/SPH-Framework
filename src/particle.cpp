cb17b2c39369ea09619512a39dbaf419a4e19da8
Mridul <dev.coffeebeans@gmail.com>
2018-08-22 18:43:44 +0530

    add getters and setters for type, mass, pressure

--- src/particle.cpp
+++ src/particle.cpp
@@ -3,11 +3,21 @@
 #include<vector>
 
 //Constructor
-Particle :: Particle(float m) {
+Particle :: Particle(float m, int type=0) {
 	this->m = m;
+	this->ptype=type;
 }
 
 // Getters
+
+float Particle :: getPressure() {
+	return this->pressure;
+}
+
+int Particle :: getType() {
+	return ptype;
+}
+
 float Particle :: getDensity() {
         return rho;
 }
@@ -28,7 +38,15 @@ float Particle :: getKernelLength() {
 	return h;
 }
 
+float Particle :: getMass() {
+	return m;
+}
+
 //Setters
+void Particle :: setPressure(float pressure) {
+	this->pressure = pressure;
+}
+
 void Particle :: setDensity(float rho) {
         this->rho = rho;
 }
--- src/particle.h
+++ src/particle.h
@@ -8,25 +8,28 @@ using namespace std;
 
 class Particle {
 
-        float rho, m, h;
+	int ptype;
+        float rho, m, h, pressure;
         pair<float, float> position;
         pair<float, float> velocity;
         pair<float, float> force;
 public:
-        Particle(float);
+        Particle(float, int);
+	void setPressure(float);
         void setDensity(float);
         void setPosition(pair<float, float>);
         void setVelocity(pair<float, float>);
         void setForce(pair<float, float>);
 	void setKernelLength(float);
 
+	float getPressure();
         float getMass();
         float getDensity();
         pair<float, float> getPosition();
         pair<float, float> getVelocity();
         pair<float, float> getForce();
 	float getKernelLength();  
-
+	int getType();
 };
 
 #endif
