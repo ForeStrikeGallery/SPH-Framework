53063269e96721e171ff2da6ac1173f28cf1a86e
Mridul <dev.coffeebeans@gmail.com>
2018-08-22 18:45:18 +0530

    add mod, add and subtract for pairs

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,16 +1,20 @@
 #include<vector>
 #include<cmath>
 #include<fstream>
+#include<iostream>
 #include "utility.h"
+using namespace std;
 
-void Utility :: saveParticleData(vector<Particle> &p) {
-        std :: ofstream file;
-        file.open("data.txt", std :: ios_base :: app);
-
+void Utility :: saveParticleData(vector<Particle> &p, string filename) {
+//        cout << "Utility Ethi";
+	std :: ofstream file;
+        //file.open(filename.c_str() , std :: ios_base :: app);
+	file.open(filename.c_str());
         for (int i=0; i< p.size(); i++) {
-                file<< p[i].getPosition().first << " " << p[i].getPosition().second<<endl;
+                file<< p[i].getPosition().first << " " << p[i].getPosition().second<<" "<<p[i].getType()<<endl;
+	//	cout<< p[i].getPosition().first << " " << p[i].getPosition().second<<" "<<p[i].getType()<<endl;
         }
-        file<< "end of time" << endl;
+        
 
         file.close();
 }
@@ -23,3 +27,15 @@ float Utility :: calcEuclideanDistance(const pair<float, float> &pos1, const pai
         
 }
 
+float Utility :: mod(pair<float, float> &pos1) {
+	return sqrt(pow(pos1.first, 2) + pow(pos1.second, 2));
+}
+
+pair<float, float> Utility :: add(pair<float, float> r1, pair<float, float> r2) {
+
+	return make_pair(r1.first + r2.first, r1.second + r2.second);
+}
+
+pair<float, float> Utility :: subtract(pair<float, float> r1, pair<float, float> r2) {
+	return make_pair(r1.first - r2.first, r1.second - r2.second);
+}
--- src/utility.h
+++ src/utility.h
@@ -1,6 +1,7 @@
 #include<vector>
 #include<iostream>
 #include<cmath>
+#include<string>
 #include "particle.h"
 using namespace std;
 #ifndef UTILITY_H
@@ -8,8 +9,11 @@ using namespace std;
 
 class Utility {
 public:
-	void saveParticleData(vector<Particle> &p);
+	void saveParticleData(vector<Particle> &p, string filename);
 	float calcEuclideanDistance(const pair<float, float> &pos1, const pair<float, float> &pos2);
+	float mod(pair<float, float>&);
+	pair<float, float> add(pair<float, float>, pair<float, float>);
+	pair<float, float> subtract(pair<float, float>, pair<float, float>);
 };
 
 #endif
